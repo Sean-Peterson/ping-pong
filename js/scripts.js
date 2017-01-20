220df571d4782f98eca9ff9ee89242d4497dc0a3
Sean Peterson <seanpeterson11@gmail.com>
2017-01-20 11:39:39 -0800

    add function that changes input into a number array up to input value

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#userInput").val();
-    var results = $("#results").append("<li>" + userInput + "</li>");
+    var userInput = $("#userInput").val()
+    var outPut = endFunction(userInput);
+    $("#results").append(outPut);
   });
+
+  var endFunction = function(n) {
+    var number = "";
+    for(var i = 1; i <= n; i += 1) {
+      number = number += i + " ";
+    };
+    return number;
+  };
+
+
 
 
 });
