00224f4dcc6349b235df3a082d5a70fa159ab9ea
Sean Peterson <seanpeterson11@gmail.com>
2017-01-20 08:38:33 -0800

    add header and label in html and printing result as answer in list form from js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var userInput = $("#userInput").val();
+    var results = $("#results").append("<li>" + userInput + "</li>");
   });
 
 
