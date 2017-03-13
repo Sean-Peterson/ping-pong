17b9a95717827b529ff3fed10d9e4189a2364284
dan Lauby and Sean Peterson <dan@>
2017-03-13 16:44:40 -0700

    refactor code

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var lib = require('bower-files')({
     }
   }
 });
+var browserSync = require('browser-sync').create();
+
 var buildProduction = utilities.env.production;
 
 
@@ -75,3 +77,28 @@ gulp.task('bowerCSS', function () {
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
+});
